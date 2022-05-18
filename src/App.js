4d52b4abe5e2bc8737258b8dd1e0e6f6cd4529a3
U-Alice <umugwanezaalice22@gmail.com>
2022-05-18 20:39:05 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './components/restoProfile';
+import Profile2 from './components/restoProfile2';
+import SignupPage from './components/signupPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen'>
+{/* <SignupPage/> */}
+<Profile />
+{/* <Profile2 /> */}
     </div>
   );
 }
