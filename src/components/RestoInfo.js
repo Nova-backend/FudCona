167194c28d1d7ce0528ffa73a0607637355d7223
ButterflyAnaise <anaiseumugwaneza1@gmail.com>
2022-05-20 12:18:45 +0200

    New Changes

--- src/components/RestoInfo.js
+++ src/components/RestoInfo.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import './RestoInfo.css';
+import search from './search.png';
+import bell from './bell.png';
+import person from './person.png';
 
 function RestoInfo() {
   return (
@@ -9,8 +12,21 @@ function RestoInfo() {
         <div className="RestoInfoContainer">
           <nav className="Navbar">
             <Link to="/" className="RestoInfo-logo">
-              <span>FudCona</span>
+                Fud<span>Cona</span>
             </Link>
+              <div className='search'>
+                <img src={search} alt='search icon'/>
+              </div>
+              <div className='bell'>
+                <img src={bell} alt='notifications icon'/>
+              </div>
+            <div className='line'></div>
+            <div className='userName'>
+              <h1>Alicia Keys</h1>
+            </div>
+            <div className='person'>
+              <img src={person} alt='user profile'/>
+            </div>
           </nav>
           <div className="left-black"></div>
           <div className="createProfile">
@@ -40,9 +56,39 @@ function RestoInfo() {
             </div>
           </div>
           <div className="restoInformation">
-            
+            <h1>Restaurant Information</h1>
+            <form className='form'>
+                <div><input type="text" placeholder="Restaurant Name" id="first"></input></div>
+                <div><input type="text" placeholder='Restaurant Complete Name' id="second"></input></div>
+                <div className='text'><label>Contacts</label></div>
+                <div>
+                  <div className="tsikeni">
+                    <p>+250</p>
+                    <span className='line2  w '></span>
+                    <div className="yvette">
+                        <input type="tel" placeholder='Mobile Number'></input>
+                    </div>
+                  </div>
+                </div>
+                <div className='text'><label>Restaurant owner details</label></div>
+                <div>
+                <div className="tsikeni">
+                    <p>+250</p>
+                    <span className='line2'></span>
+                    <div className="yvette">
+                        <input type="tel" placeholder='Mobile Number'></input>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div><input type="text" placeholder="Restaurant Name" id="fifth"></input></div>
+            </form> 
           </div>
         </div>
+
+
+        
       </div>
     </>
   )
